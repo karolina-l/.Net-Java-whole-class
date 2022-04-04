b817ba33be03c9f0fbb88adc38800b09dd914584
karolina-l <57325023+karolina-l@users.noreply.github.com>
2022-04-04 22:39:55 +0200

    API connection done, currency calculation done

---
+++ P2/API/API/Currency.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace API
+{
+    public class Currency
+    {
+        //wszystko co jest w tej klasie jest spisane z neta, bo to już był szczyt moich możliwości wczoraj.
+        //Nawet nie do końca wiem jak to działa XD
+        private readonly String BASE_URI = "https://free.currconv.com/";
+        private readonly String API_VERSION = "v7";
+
+        public Decimal GetCurrencyExchange(String localCurrency, String foreignCurrency)
+        {
+            var code = $"{localCurrency}_{foreignCurrency}";
+            var newRate = FetchSerializedData(code);
+            return newRate;
+        }
+
+        public Decimal FetchSerializedData(String code)
+        {
+            var url = $"{BASE_URI}/api/{API_VERSION}/convert?q={code}&compact=y";
+            var webClient = new WebClient();
+            var jsonData = String.Empty;
+
+            var conversionRate = 1.0m;
+            try
+            {
+                jsonData = webClient.DownloadString(url);
+                var jsonObject = new JavaScriptSerializer().Deserialize<Dictionary<string, Dictionary<string, decimal>>>(jsonData);
+                var result = jsonObject[code];
+                conversionRate = result["val"];
+
+            }
+            catch (Exception) { }
+
+            return conversionRate;
+        }
+    }
+}
--- P2/API/API/Form1.cs
+++ P2/API/API/Form1.cs
@@ -10,60 +10,86 @@ using System.Windows.Forms;
 using System.IO;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+using System.Net;
 
 namespace API
 {
     public partial class Form1 : Form
     {
-        public List<Student> students;
-        //public System.Windows.Forms.MonthCalendar monthCalendar1;
-        public System.Windows.Forms.TextBox textBox1;
-        public string date, end;
+        public string date, base_currency, final_currency, amt;
+        public double amount;
+
         public Form1()
         {
             InitializeComponent();
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar(); 
-            students = new List<Student>();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            /*string json = File.ReadAllText("student.json");
-            List<Student> students = JsonConvert.DeserializeObject<List<Student>>(json);
-            listBox1.Items.Clear();
-            foreach (Student student in students)
-                listBox1.Items.Add(student.ToString());*/
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            date = textBox2.Text;
         }
 
-        public void button1_Click(object sender, EventArgs e)
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
-            getResponseAsync();
-            foreach (Student student in students)
-                listBox1.Items.Add(student.ToString());
-        }
-        public async Task<string> getResponseAsync()
-        {
-            HttpClient client = new HttpClient();
-            string call = "http://radoslaw.idzikowski.staff.iiar.pwr.wroc.pl/instruction/students.json";
-            string response = await client.GetStringAsync(call);
-            students = JsonConvert.DeserializeObject<List<Student>>(response);
-            return response;
-        }
-        public void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
-        {
-            monthCalendar1.MaxSelectionCount = 1;
-            monthCalendar1.ShowToday = false;
-            monthCalendar1.ShowTodayCircle = false;
-            date = monthCalendar1.SelectionStart.ToString("yyyy-MM-dd");
-            end = monthCalendar1.SelectionEnd.ToString("yyyy-MM-dd");
-            MessageBox.Show("DateSelected: " + date);
+            final_currency = textBox3.Text;
         }
 
-        public void textBox2_TextChanged(object sender, EventArgs e)
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            textBox2.Clear();
+            date = DateTime.Now.ToString("yyyy-MM-dd");
             textBox2.Text = date;
+        }
+
+        private void textBox4_TextChanged_1(object sender, EventArgs e)
+        {
+            amt = textBox4.Text;
+            amount = Convert.ToDouble(amt);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            base_currency = textBox1.Text;
+        }
+
+        public async void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            var dat = getValue();
+            var d = Convert.ToDouble(dat);
+            var conversionRate = 1.0m;
+            var result = d * amount;
+            textBox6.Text = result.ToString();
+            //listBox1.Items.Clear();
+            //listBox1.Items.Add(dat.ToString());
+        }
+
+        public decimal getValue()
+        {
+            var code = $"{base_currency}_{final_currency}";
+            var conversionRate = 4.0m;
+
+            //łączenie z api
+            var json = String.Empty;
+            var webClient = new WebClient();
+            string call = "https://free.currconv.com/api/v7/convert?q=" + base_currency + "_" + final_currency + "," + final_currency + "_" + base_currency + "&compact=ultra&date=" + date + "&apiKey=6e518d133c379bc395df";
+            json = webClient.DownloadString(call);
+
+            //testy na pliku
+            //string json = File.ReadAllText("student.json");
+            
+            textBox5.Text = json;
+            var jsonObject = new JavaScriptSerializer().Deserialize<Dictionary<string, Dictionary<string, decimal>>>(json);
+            var result = jsonObject[code];
+            conversionRate = result[date];
+
+            return conversionRate;
         }
     }
 }
